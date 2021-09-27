40b22deab824d21288b646d259791772a0115d90
schollsebastian <scholl.sebastian@gmx.at>
2021-09-27 20:07:38 +0200

    Log all messages on the console

--- logger/src/index.ts
+++ logger/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+import mqtt from 'mqtt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = mqtt.connect(`mqtt://${process.env['MQTT_BROKER_HOSTNAME']}`, { clientId: 'logger' });
+
+client.on('connect', () => {
+  console.log('Connected to MQTT Broker');
+
+  client.subscribe('#', error => {
+    if (error) {
+      console.error(error);
+    }
+  });
+});
+
+client.on('message', (topic, message) => {
+  console.log(topic, message.toString());
+});
