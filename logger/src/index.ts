e8328a547c1965f38bee95bbaecf997a135fe13c
schollsebastian <scholl.sebastian@gmx.at>
2021-09-27 20:38:29 +0200

    Rename client to mqttClient

--- logger/src/index.ts
+++ logger/src/index.ts
@@ -3,18 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = mqtt.connect(`mqtt://${process.env['MQTT_BROKER_HOSTNAME']}`, { clientId: 'logger' });
+const mqttClient = mqtt.connect(`mqtt://${process.env['MQTT_BROKER_HOSTNAME']}`, { clientId: 'logger' });
 
-client.on('connect', () => {
+mqttClient.on('connect', () => {
   console.log('Connected to MQTT Broker');
 
-  client.subscribe('#', error => {
+  mqttClient.subscribe('#', error => {
     if (error) {
       console.error(error);
     }
   });
 });
 
-client.on('message', (topic, message) => {
+mqttClient.on('message', (topic, message) => {
   console.log(topic, message.toString());
 });
